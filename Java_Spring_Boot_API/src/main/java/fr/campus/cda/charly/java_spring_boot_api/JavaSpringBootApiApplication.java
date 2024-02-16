ca2966d07b7acf7dd6a8aff1d8784fc633622901
CharlyLstudent <charly.lecable@le-campus-numerique.fr>
2024-02-16 09:59:55 +0100

    first commit after first compile

---
+++ Java_Spring_Boot_API/src/main/java/fr/campus/cda/charly/java_spring_boot_api/JavaSpringBootApiApplication.java
@@ -0,0 +1,12 @@
+package fr.campus.cda.charly.java_spring_boot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootApiApplication.class, args);
+    }
+}
---
+++ Java_Spring_Boot_API/src/test/java/fr/campus/cda/charly/java_spring_boot_api/JavaSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.campus.cda.charly.java_spring_boot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
